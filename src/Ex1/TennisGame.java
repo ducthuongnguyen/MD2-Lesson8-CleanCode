13ed73f4936991de40e963f78d5fad90fb0062f7
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-03-30 21:41:22 +0700

    first

---
+++ src/Ex1/TennisGame.java
@@ -0,0 +1,62 @@
+package Ex1;
+
+public class TennisGame {
+    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
+        String result = "";
+        int tempScore=0;
+        if (player1Score==player2Score)
+        {
+            switch (player1Score)
+            {
+                case 0:
+                    result = "Love-All";
+                    break;
+                case 1:
+                    result = "Fifteen-All";
+                    break;
+                case 2:
+                    result = "Thirty-All";
+                    break;
+                case 3:
+                    result = "Forty-All";
+                    break;
+                default:
+                    result = "Deuce";
+                    break;
+
+            }
+        }
+        else if (player1Score>=4 || player2Score>=4)
+        {
+            int minusResult = player1Score-player2Score;
+            if (minusResult==1) result ="Advantage player1";
+            else if (minusResult ==-1) result ="Advantage player2";
+            else if (minusResult>=2) result = "Win for player1";
+            else result ="Win for player2";
+        }
+        else
+        {
+            for (int i=1; i<3; i++)
+            {
+                if (i==1) tempScore = player1Score;
+                else { result+="-"; tempScore = player2Score;}
+                switch(tempScore)
+                {
+                    case 0:
+                        result+="Love";
+                        break;
+                    case 1:
+                        result+="Fifteen";
+                        break;
+                    case 2:
+                        result+="Thirty";
+                        break;
+                    case 3:
+                        result+="Forty";
+                        break;
+                }
+            }
+        }
+        return result;
+    }
+}
